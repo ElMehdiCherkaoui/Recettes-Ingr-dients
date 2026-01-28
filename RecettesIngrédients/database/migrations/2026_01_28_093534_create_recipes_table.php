0b7f3fb48252f2d5fe83f81a64650a72ea05bd56
ElMehdiCherkaoui <cherkaouim451@gmail.com>
2026-01-28 16:54:15 +0100

    feat : add migration for recipes

---
+++ RecettesIngrédients/database/migrations/2026_01_28_093534_create_recipes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->string("description");
+            $table->string("steps");
+            $table->string("image");
+            $table->boolean("isRecipeOfDay");
+            $table->timestamps();
+            $table->foreignId('users_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('categories_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('ingredients_id')->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recipes');
+    }
+};
