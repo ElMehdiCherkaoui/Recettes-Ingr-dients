7143701a4e9dcf33b27b44dddb025b5e442e7537
ElMehdiCherkaoui <cherkaouim451@gmail.com>
2026-01-28 16:52:49 +0100

    feat : add factory ingredient

---
+++ RecettesIngrédients/database/factories/IngredientFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Ingredient>
+ */
+class IngredientFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->sentence(6),
+            'quantity' => fake()->numberBetween(0, 50),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+    }
+}
