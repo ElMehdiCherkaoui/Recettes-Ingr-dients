089f8d2b772452dff86cd8c23fd8bd1fcdb032df
ElMehdiCherkaoui <cherkaouim451@gmail.com>
2026-01-28 16:50:24 +0100

    feat : add favorite model

---
+++ RecettesIngrédients/app/Models/Favorite.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Favorite extends Model
+{
+    use HasFactory;
+
+
+    protected $fillable = [
+        'users_id',
+        'categories_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'users_id');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'categories_id');
+    }
+}
