9026600e6b2202768c4b660e1eb003908dbc32f1
ElMehdiCherkaoui <cherkaouim451@gmail.com>
2026-01-28 16:58:27 +0100

    feat : add router post to rout in controller

--- RecettesIngrédients/routes/web.php
+++ RecettesIngrédients/routes/web.php
@@ -23,52 +23,41 @@ use App\Http\Controllers\recipes\recipesCreateController;
 use App\Http\Controllers\recipes\recipesPdfController;
 use App\Http\Controllers\comments\indexCommentsController;
 
-Route::get('/admin/dashboard', [AdminDashboardController::class, 'index'])
-    ->name('admin.dashboard');
+Route::get('/admin/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
 
-Route::get('/admin/recipes', [AdminRecipesController::class, 'index'])
-    ->name('admin.recipes');
+Route::get('/admin/recipes', [AdminRecipesController::class, 'index'])->name('admin.recipes');
 
-Route::get('/admin/recipe-of-the-day', [AdminRecipeOfTheDayController::class, 'index'])
-    ->name('admin.recipe-of-the-day');
+Route::get('/admin/recipe-of-the-day', [AdminRecipeOfTheDayController::class, 'index'])->name('admin.recipe-of-the-day');
 
-Route::get('/login', [authLoginController::class, 'index'])
-    ->name('auth.login');
+Route::get('/login', [authLoginController::class, 'index'])->name('auth.login');
 
-Route::get('/register', [authRegisterController::class, 'index'])
-    ->name('auth.register');
+Route::post('/login', [authLoginController::class, 'login'])->name('login.submit');
 
+Route::post('/logout', [AuthLoginController::class, 'logout'])->name('logout');
 
-Route::get('/profile', [userProfileController::class, 'index'])
-    ->name('profile.index');
+Route::get('/register', [authRegisterController::class, 'index'])->name('auth.register');
 
-Route::get('/profile-recipes', [userRecipesController::class, 'index'])
-    ->name('profile.recipes');
+Route::post('/register', [authRegisterController::class, 'register'])->name('register.submit');
 
-Route::get('/profile-favorites', [userFavoritesController::class, 'index'])
-    ->name('profile.favorites');
+Route::get('/profile', [userProfileController::class, 'index'])->name('profile.index');
 
-Route::get('/profile-comments', [userCommentsController::class, 'index'])
-    ->name('profile.comments');
+Route::get('/profile-recipes', [userRecipesController::class, 'index'])->name('profile.recipes');
 
-Route::get('/recipes', [recipesDashboardController::class, 'index'])
-    ->name('recipe.index');
+Route::get('/profile-favorites', [userFavoritesController::class, 'index'])->name('profile.favorites');
 
-Route::get('/recipes-show', [recipesDetailsController::class, 'index'])
-    ->name('recipe.details');
+Route::get('/profile-comments', [userCommentsController::class, 'index'])->name('profile.comments');
 
+Route::get('/recipes', [recipesDashboardController::class, 'index'])->name('recipe.index');
 
-Route::get('/recipes-edit', [recipesEditController::class, 'index'])
-    ->name('recipe.edit');
+Route::get('/recipes-show', [recipesDetailsController::class, 'index'])->name('recipe.details');
 
-Route::get('/recipes-create', [recipesCreateController::class, 'index'])
-    ->name('recipe.create');
+Route::get('/recipes-edit', [recipesEditController::class, 'index'])->name('recipe.edit');
 
-Route::get('/recipes-pdf', [recipesPdfController::class, 'index'])
-    ->name('recipe.pdf');
+Route::get('/recipes-create', [recipesCreateController::class, 'index'])->name('recipe.create');
 
-Route::get('/comments', [indexCommentsController::class, 'index'])
-    ->name('comments.index');
+Route::get('/recipes-pdf', [recipesPdfController::class, 'index'])->name('recipe.pdf');
+
+Route::get('/comments', [indexCommentsController::class, 'index'])->name('comments.index');
 
 Route::get('/', function () {
     return view('home.index');
