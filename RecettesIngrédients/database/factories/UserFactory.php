ae8d77718a6a633c6f7226cf233d50d12f997480
ElMehdiCherkaoui <cherkaouim451@gmail.com>
2026-01-28 16:53:08 +0100

    feat : add factory user

--- RecettesIngrédients/database/factories/UserFactory.php
+++ RecettesIngrédients/database/factories/UserFactory.php
@@ -21,14 +21,19 @@ class UserFactory extends Factory
      *
      * @return array<string, mixed>
      */
+
+
+    protected $model = \App\Models\User::class;
+    
     public function definition(): array
     {
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+
         ];
     }
 
@@ -37,7 +42,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
