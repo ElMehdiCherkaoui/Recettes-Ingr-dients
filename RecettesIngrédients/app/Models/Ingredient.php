15f5e60d6ed74310dce08c88c3d32f2c14455d92
ElMehdiCherkaoui <cherkaouim451@gmail.com>
2026-01-28 16:50:48 +0100

    feat : add ingredient model

---
+++ RecettesIngrédients/app/Models/Ingredient.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
+class Ingredient extends Model
+{
+    use HasFactory;
+
+
+    protected $fillable = [
+        'name',
+        'quantity',
+    ];
+
+
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => ucfirst($value),
+            set: fn($value) => strtolower($value),
+        );
+    }
+}
