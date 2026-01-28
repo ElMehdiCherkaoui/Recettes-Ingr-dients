b57560fa84428b05b7e3e1805df8bfe2a47a290f
ElMehdiCherkaoui <cherkaouim451@gmail.com>
2026-01-28 16:51:05 +0100

    feat : add recipes model

---
+++ RecettesIngrédients/app/Models/Recipes.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
+class Recipes extends Model
+{
+    use HasFactory;
+
+
+    protected $fillable = [
+        'title',
+        'description',
+        'steps',
+        'image',
+        'isRecipeOfDay',
+        'users_id',
+        'categories_id',
+    ];
+
+
+    protected $casts = [
+        'isRecipeOfDay' => 'boolean',
+    ];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'users_id');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'categories_id');
+    }
+
+    public function ingredients()
+    {
+        return $this->belongsTo(Ingredient::class, 'ingredients_id');
+    }
+
+    protected function title(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => ucfirst($value),
+            set: fn ($value) => strtolower($value),
+        );
+    }
+}
