4b05555d316d2c5dd3b8547f6bb3b08831fa42a5
ElMehdiCherkaoui <cherkaouim451@gmail.com>
2026-01-28 16:49:17 +0100

    feat : modify UserProfileController

--- RecettesIngrédients/app/Http/Controllers/User/userProfileController.php
+++ RecettesIngrédients/app/Http/Controllers/User/userProfileController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class userProfileController extends Controller
 {
     public function index()
     {
-        return view('profile.index');
+        $user = Auth::user(); 
+        return view('profile.index', compact('user'));
     }
 }
