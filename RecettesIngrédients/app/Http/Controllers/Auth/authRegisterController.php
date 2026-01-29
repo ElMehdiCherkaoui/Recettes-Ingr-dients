b40ad6aa70a075fa15006797deb967c3034b3b5f
ElMehdiCherkaoui <cherkaouim451@gmail.com>
2026-01-29 17:03:30 +0100

    feat : update register controller

--- RecettesIngrédients/app/Http/Controllers/Auth/authRegisterController.php
+++ RecettesIngrédients/app/Http/Controllers/Auth/authRegisterController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Facades\Auth;
 
 class AuthRegisterController extends Controller
@@ -21,18 +20,11 @@ class AuthRegisterController extends Controller
     public function register(Request $request)
     {
 
-        $validated = $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|email|max:255|unique:users,email',
-            'password' => 'required|string|min:6|confirmed', 
-        ]);
-
-
         $user = User::create([
-            'name' => $validated['name'],
-            'email' => $validated['email'],
-            'password' => Hash::make($validated['password']),
+            'name' => $request['name'],
+            'email' => $request['email'],
             'role' => 'user',
+            'password' => Hash::make($request['password']),
         ]);
 
         Auth::login($user);
