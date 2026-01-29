454798e5c04f30c7fefbb04a09ce5dbbee24f2ee
ElMehdiCherkaoui <cherkaouim451@gmail.com>
2026-01-29 17:04:03 +0100

    feat : add recipe create function to create an recipe

--- RecettesIngrédients/app/Http/Controllers/Recipes/recipesCreateController.php
+++ RecettesIngrédients/app/Http/Controllers/Recipes/recipesCreateController.php
@@ -4,11 +4,34 @@ namespace App\Http\Controllers\Recipes;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Ingredient;
+use App\Models\Recipes;
+use Illuminate\Support\Facades\Auth;
 
 class recipesCreateController extends Controller
 {
     public function index()
     {
-        return view('recipes.create');
+        return view('recipes.create', [
+            'categories' => Category::all(),
+            'ingredients' => Ingredient::all(),
+        ]);
+    }
+    public function store(Request $request)
+    {
+
+        Recipes::create([
+            'title'          => $request['title'],
+            'description'    => $request['description'],
+            'steps'          => $request['steps'],
+            'image'          => $request['image'],
+            'isRecipeOfDay'  => false,
+            'users_id'       => Auth::id(),
+            'categories_id'  => $request['categories_id'],
+            'ingredients_id' => $request['ingredients_id'],
+        ]);
+
+        return redirect()->route('profile.index');
     }
 }
