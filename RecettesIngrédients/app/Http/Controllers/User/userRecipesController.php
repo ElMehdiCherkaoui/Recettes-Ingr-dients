8222827da87e30b16b04c1d69e22fc1ed8b3b592
ElMehdiCherkaoui <cherkaouim451@gmail.com>
2026-01-29 17:05:56 +0100

    feat : add destory in recipe controller to delete and recipe

--- RecettesIngrédients/app/Http/Controllers/User/userRecipesController.php
+++ RecettesIngrédients/app/Http/Controllers/User/userRecipesController.php
@@ -3,12 +3,25 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Ingredient;
+use App\Models\Recipes;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class userRecipesController extends Controller
 {
     public function index()
     {
-        return view('profile.recipes');
+        $recipes = Recipes::with('category')->where('users_id', Auth::id())->latest()->get();
+
+        return view('profile.recipes', compact('recipes'));
+    }
+
+    public function destroy(Recipes $recipe)
+    {
+        $recipe->delete();
+
+        return redirect()->route('profile-recipes.index');
     }
 }
