3a5a581a698a5de42526c47b9f5e43055974942f
ElMehdiCherkaoui <cherkaouim451@gmail.com>
2026-01-29 17:02:38 +0100

    feat : add function in controller for recipeoftheday to add and update it

--- RecettesIngrédients/app/Http/Controllers/Admin/AdminRecipeOfTheDayController.php
+++ RecettesIngrédients/app/Http/Controllers/Admin/AdminRecipeOfTheDayController.php
@@ -3,11 +3,26 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Recipes;
+use Illuminate\Http\Request;
 
 class AdminRecipeOfTheDayController extends Controller
 {
     public function index()
     {
-        return view('admin.recipe-of-the-day');
+        $recipes = Recipes::with(['user', 'category'])->orderByDesc('isRecipeOfDay')->get();
+
+        $recipeOfDay = Recipes::with(['user', 'category'])->where('isRecipeOfDay', true)->first();
+
+        return view('admin.recipe-of-the-day', compact('recipes', 'recipeOfDay'));
+    }
+    public function update(Request $request)
+    {
+
+        Recipes::where('isRecipeOfDay', true)->update(['isRecipeOfDay' => false]);
+
+        Recipes::where('id', $request->recipe_id)->update(['isRecipeOfDay' => true]);
+
+        return redirect()->route('admin.recipe-of-the-day');
     }
 }
