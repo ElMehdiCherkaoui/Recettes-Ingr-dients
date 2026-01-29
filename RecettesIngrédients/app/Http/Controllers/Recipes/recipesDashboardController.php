647c58d98cf456e09fb0b8195ea650cd39d72429
ElMehdiCherkaoui <cherkaouim451@gmail.com>
2026-01-29 17:04:31 +0100

    feat add dashboard to list and filter the recipes also searching

--- RecettesIngrédients/app/Http/Controllers/Recipes/recipesDashboardController.php
+++ RecettesIngrédients/app/Http/Controllers/Recipes/recipesDashboardController.php
@@ -2,13 +2,43 @@
 
 namespace App\Http\Controllers\Recipes;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Ingredient;
+use App\Models\Recipes;
 
 class recipesDashboardController extends Controller
 {
+
+
     public function index()
     {
-        return view('recipes.index');
+
+        $categories = Category::all();
+        $ingredients = Ingredient::all();
+        $recipes = Recipes::with(['category', 'ingredient', 'user'])->latest()->get();
+
+        return view('recipes.index', compact('recipes', 'categories', 'ingredients'));
+    }
+
+    public function filter(Request $request)
+    {
+        $categories = Category::all();
+        $ingredients = Ingredient::all();
+
+        $query = Recipes::query();
+
+        if ($request->filled('search')) {
+            $query->where('title', 'LIKE', '%' . $request->search . '%');
+        }
+
+        if ($request->filled('category')) {
+            $query->where('categories_id', $request->category);
+        }
+
+        $recipes = $query->orderBy('created_at', 'desc')->get();
+
+        return view('recipes.index', compact('recipes', 'categories', 'ingredients'));
     }
 }
