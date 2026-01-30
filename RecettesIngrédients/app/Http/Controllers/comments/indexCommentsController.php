2e50003585c5b7ab16b5072a33f7c9ccdaae9dcd
ElMehdiCherkaoui <cherkaouim451@gmail.com>
2026-01-30 21:31:06 +0100

    feat : add comment stor function

--- RecettesIngrédients/app/Http/Controllers/comments/indexCommentsController.php
+++ RecettesIngrédients/app/Http/Controllers/comments/indexCommentsController.php
@@ -3,12 +3,25 @@
 namespace App\Http\Controllers\comments;
 
 use App\Http\Controllers\Controller;
-
+use App\Models\Comment;
+use App\Models\Recipes;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class indexCommentsController extends Controller
 {
-    public function index()
+    public function store(Request $request, Recipes $recipe)
     {
-        return view('comments.index');
+        $request->validate([
+            'content' => 'required|string|max:1000',
+        ]);
+
+        Comment::create([
+            'content'   => $request->content,
+            'users_id'   => Auth::id(),
+            'recipes_id' => $recipe->id,
+        ]);
+
+        return redirect()->back();
     }
 }
